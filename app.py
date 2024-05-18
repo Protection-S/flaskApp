9a3d494e2f0422c21a6681c3272490c66fc13214
MaXimka <115403109+Protection-S@users.noreply.github.com>
2024-05-18 15:03:10 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hi')
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=9999)
